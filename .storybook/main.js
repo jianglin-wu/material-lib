4538ab3ac16cc947836e9f31ba04126810ccf558
jianglin-wu <wjiangl.id@gmail.com>
2022-07-17 00:17:54 +0800

    fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,9 @@
 module.exports = {
-  // stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/preset-create-react-app',
-    // '@storybook/addon-links',
-    // '@storybook/addon-essentials',
-    // '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  // framework: '@storybook/react',
+  framework: '@storybook/react',
 };
