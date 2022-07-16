40dd8fc358851b3395aa0fe85a27be02dd8decf1
jianglin-wu <wjiangl.id@gmail.com>
2022-07-17 00:04:44 +0800

    fix: ignore

--- src/components/Todo/TaskList.test.tsx
+++ src/components/Todo/TaskList.test.tsx
@@ -1,16 +1,14 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { IProps } from './TaskList';
 import { WithPinnedTasks } from './TaskList.stories';
 
 it('renders pinned tasks at the start of the list', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
-    <WithPinnedTasks {...(WithPinnedTasks.args as IProps)} />,
-    div,
-  );
+  const root = ReactDOM.createRoot(div);
+  root.render(<WithPinnedTasks {...(WithPinnedTasks.args as IProps)} />);
   const lastTaskInput = div.querySelector(
     '.list-item:nth-child(1) input[value="Task 4"]',
   );
   expect(lastTaskInput).not.toBe(null);
-  ReactDOM.unmountComponentAtNode(div);
+  root.unmount();
 });
