2e60ceb830a1514f3f9fffe7a443d50c6e1ae795
jianglin-wu <wjiangl.id@gmail.com>
2022-07-19 19:35:38 +0800

    feat: add link

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 const Home = React.lazy(() => import('./pages/Home'));
 const Todo = React.lazy(() => import('./pages/Todo'));
+const Form = React.lazy(() => import('./pages/Form'));
 
 const Loading = () => <div>loading</div>;
 const asyncElement = (element: React.ReactNode) => (
@@ -14,7 +15,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={asyncElement(<Home />)} />
-      <Route path="todo" element={asyncElement(<Todo />)} />
+      <Route path="/todo" element={asyncElement(<Todo />)} />
+      <Route path="/form" element={asyncElement(<Form />)} />
     </Routes>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,62 +1,17 @@
-import AdvancedForm from '../../components/AdvancedForm';
-
+import { Link } from 'react-router-dom';
 function Home() {
-  const formItemProps = {
-    labelCol: {
-      span: 8,
-    },
-    wrapperCol: {
-      span: 16,
-    },
-  };
-  const fromItems = [
-    {
-      label: '国家',
-      field: 'country',
-      type: 'select',
-      formItemProps,
-    },
-    {
-      label: '省份',
-      field: 'province',
-      type: 'select',
-      formItemProps,
-      dependencies: ['country'],
-    },
-    {
-      label: '城市',
-      field: 'city',
-      type: 'select',
-      formItemProps,
-      dependencies: ['province'],
-    },
-    {
-      label: '区县',
-      field: 'region',
-      type: 'select',
-      formItemProps,
-      dependencies: ['city'],
-    },
-    {
-      label: '街道',
-      field: 'street',
-      type: 'select',
-      formItemProps,
-      dependencies: ['region'],
-    },
-  ];
   return (
-    <AdvancedForm
-      formItems={[
-        {
-          type: 'grid',
-          colProps: {
-            span: 8,
-          },
-          children: fromItems,
-        },
-      ]}
-    />
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/todo">Todo</Link>
+      </li>
+      <li>
+        <Link to="/form">Form</Link>
+      </li>
+    </ul>
   );
 }
 
